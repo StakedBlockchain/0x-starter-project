de2c210ff7ccad769ca6d26487db5e3ad09f956f
Sota Watanabe <w.souta.s.life@gmail.com>
2019-01-08 11:49:27 +0900

    add

--- react-view/src/views/Home/Home.jsx
+++ react-view/src/views/Home/Home.jsx
@@ -2,6 +2,11 @@ import React from "react";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 import InputLabel from "@material-ui/core/InputLabel";
+import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+
 // core components
 import GridItem from "components/Grid/GridItem.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
@@ -14,165 +19,183 @@ import CardBody from "components/Card/CardBody.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
 
 import avatar from "assets/img/faces/marc.jpg";
+import { CardContent, Avatar, Grid, CardActionArea, CardMedia, CardActions } from "@material-ui/core";
 
-const styles = {
-  cardCategoryWhite: {
-    color: "rgba(255,255,255,.62)",
-    margin: "0",
-    fontSize: "14px",
-    marginTop: "0",
-    marginBottom: "0"
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
   },
-  cardTitleWhite: {
-    color: "#FFFFFF",
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none"
-  }
-};
 
-function Home(props) {
+  card: {
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+
+  media: {
+    height: 250,
+  },
+});
+
+function PaperSheet(props) {
   const { classes } = props;
+
   return (
     <div>
-      <GridContainer>
-        <GridItem xs={12} sm={12} md={8}>
-          <Card>
-            <CardHeader color="primary">
-              <h4 className={classes.cardTitleWhite}>Edit Profile</h4>
-              <p className={classes.cardCategoryWhite}>Complete your profile</p>
-            </CardHeader>
-            <CardBody>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={5}>
-                  <CustomInput
-                    labelText="Company (disabled)"
-                    id="company-disabled"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                    inputProps={{
-                      disabled: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={3}>
-                  <CustomInput
-                    labelText="Username"
-                    id="username"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Email address"
-                    id="email-address"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="First Name"
-                    id="first-name"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="Last Name"
-                    id="last-name"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="City"
-                    id="city"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Country"
-                    id="country"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Postal Code"
-                    id="postal-code"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={12}>
-                  <InputLabel style={{ color: "#AAAAAA" }}>About me</InputLabel>
-                  <CustomInput
-                    labelText="Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
-                    id="about-me"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                    inputProps={{
-                      multiline: true,
-                      rows: 5
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-            </CardBody>
-            <CardFooter>
-              <Button color="primary">Update Profile</Button>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={12} md={4}>
-          <Card profile>
-            <CardAvatar profile>
-              <a href="#pablo" onClick={e => e.preventDefault()}>
-                <img src={avatar} alt="..." />
-              </a>
-            </CardAvatar>
-            <CardBody profile>
-              <h6 className={classes.cardCategory}>CEO / CO-FOUNDER</h6>
-              <h4 className={classes.cardTitle}>Alec Thompson</h4>
-              <p className={classes.description}>
-                Don't be scared of the truth because we need to restart the
-                human foundation in truth And I love you like Kanye loves Kanye
-                I love Rick Owens’ bed design but the back is...
-              </p>
-              <Button color="primary" round>
-                Follow
-              </Button>
-            </CardBody>
-          </Card>
-        </GridItem>
+      <GridContainer className={classes.root} elevation={1}>
+        <GridContainer xs={12}>
+          <GridItem xs={12} sm={12}>
+            <Card className={classes.card}>
+              <CardHeader>
+                <Typography>
+                  <h1>Digital Assets For Everyone. 〜分散型取引所をすべての人に〜</h1>
+                  <h4>【テストネット版】ブロックチェーンを応用し、秘密鍵を自分で管理しながら、集権的な第三者を通さずにウォレット間で取引を実現します。</h4>
+                </Typography>
+              </CardHeader>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      
+        <GridContainer xs={12} sm={12}>
+          <GridItem xs={12}>
+            <Card className={classes.card}>
+              <CardHeader>
+                <Typography><h2>なぜ分散型取引所が重要なのか？</h2></Typography>
+              </CardHeader>
+              <CardContent>
+                <GridContainer>
+                  <GridItem xs={12} sm={6}>
+                    <GridContainer>
+                     <Card className={classes.card}>
+                       <CardActionArea>
+                        <CardMedia
+                          className={classes.media}
+                          image="img/wallet.jpg"
+                          title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            ウォレット間の取引
+                          </Typography>
+                          <Typography component="p">
+                            分散型取引所では、既存の取引所のような集権的な機関を通すことなくスマートコントラクトを通して送信者と受信者の間でトラストレスに交換を行うことができます。
+                          </Typography>
+                        </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                         <Button size="small" color="primary">
+                           もっと学ぶ
+                         </Button>
+                        </CardActions>
+                    </Card>
+                    </GridContainer>
+                  </GridItem>
+
+                  <GridItem xs={12} sm={6}>
+                    <GridContainer>
+                     <Card className={classes.card}>
+                       <CardActionArea>
+                        <CardMedia
+                          className={classes.media}
+                          image="img/secure.jpg"
+                          title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            オーナーシップ
+                          </Typography>
+                          <Typography component="p">
+                            既存の取引所ではユーザーの資産を預かる形で管理しています。分散取引所ではユーザーが自分の資産を管理することができるため、ハッキングリスクが軽減します。
+                          </Typography>
+                        </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                         <Button size="small" color="primary">
+                           もっと学ぶ
+                         </Button>
+                        </CardActions>
+                    </Card>
+                    </GridContainer>
+                  </GridItem>
+
+                  <GridItem xs={12} sm={6}>
+                    <GridContainer>
+                     <Card className={classes.card}>
+                       <CardActionArea>
+                        <CardMedia
+                          className={classes.media}
+                          image="img/borderless.jpg"
+                          title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            ボーダレスな取引
+                          </Typography>
+                          <Typography component="p">
+                            分散取引所には国境がありません。需給に応じてニーズがマッチする人と取引ができます。
+                          </Typography>
+                        </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                         <Button size="small" color="primary">
+                           もっと学ぶ
+                         </Button>
+                        </CardActions>
+                    </Card>
+                    </GridContainer>
+                  </GridItem>
+
+                  <GridItem xs={12} sm={6}>
+                    <GridContainer>
+                     <Card className={classes.card}>
+                       <CardActionArea>
+                        <CardMedia
+                          className={classes.media}
+                          image="img/multipletokens.jpg"
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            早い、安い
+                          </Typography>
+                          <Typography component="p">
+                          既存の取引所に比べ分散型取引所の手数料は安く、流動性に応じて早く決済を行うことができます。
+                          </Typography>
+                        </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                         <Button size="small" color="primary">
+                           もっと学ぶ
+                         </Button>
+                        </CardActions>
+                    </Card>
+                    </GridContainer>
+                  </GridItem>
+
+                </GridContainer>
+              </CardContent>
+            </Card>
+          </GridItem>
+        </GridContainer>
+
+        <GridContainer xs={12}>
+          <GridItem xs={12}　sm={12}>
+            <Card className={classes.card}>
+              <CardHeader>
+                <Typography>
+                  <h2>使用方法</h2>
+                </Typography>
+              </CardHeader>
+            </Card>
+          </GridItem>
+        </GridContainer>
       </GridContainer>
     </div>
   );
 }
 
-export default withStyles(styles)(Home);
+PaperSheet.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(PaperSheet);
